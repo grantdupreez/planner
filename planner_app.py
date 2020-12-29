1af42ecf60c28c0a4d87694e802613a47ede9ca3
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2020-12-29 14:40:44 +0000

    Update planner_app.py

--- planner_app.py
+++ planner_app.py
@@ -7,8 +7,10 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, header=[0], parse_dates=[1])
     st.write(df)
     
+#    fig = ff.create_gantt(df, index_col='Status', show_colorbar=True,
+#                         group_tasks=True, showgrid_x=True, showgrid_y=True)
     fig = ff.create_gantt(df, index_col='Status', show_colorbar=True,
-                         group_tasks=True, showgrid_x=True, showgrid_y=True)
+                         showgrid_x=True, showgrid_y=True)
  
     fig
     
