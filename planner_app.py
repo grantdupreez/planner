c89929b4e4d662cc688f633e574c115c042269bd
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2020-12-29 12:58:13 +0000

    Update planner_app.py

--- planner_app.py
+++ planner_app.py
@@ -1,13 +1,20 @@
+import pandas as pd
 import plotly.figure_factory as ff
 
-df = [dict(Task="Job-1", Start='2017-01-01', Finish='2017-02-02', Resource='Complete'),
-      dict(Task="Job-1", Start='2017-02-15', Finish='2017-03-15', Resource='Incomplete'),
-      dict(Task="Job-2", Start='2017-01-17', Finish='2017-02-17', Resource='Not Started'),
-      dict(Task="Job-2", Start='2017-01-17', Finish='2017-02-17', Resource='Complete'),
-      dict(Task="Job-3", Start='2017-03-10', Finish='2017-03-20', Resource='Not Started'),
-      dict(Task="Job-3", Start='2017-04-01', Finish='2017-04-20', Resource='Not Started'),
-      dict(Task="Job-3", Start='2017-05-18', Finish='2017-06-18', Resource='Not Started'),
-      dict(Task="Job-4", Start='2017-01-14', Finish='2017-03-14', Resource='Complete')]
+uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    df = pd.read_csv(uploaded_file, header=[0], parse_dates=[0])
+    st.write(df)
+
+#df = [dict(Task="Job-1", Start='2017-01-01', Finish='2017-02-02', Resource='Complete'),
+#      dict(Task="Job-1", Start='2017-02-15', Finish='2017-03-15', Resource='Incomplete'),
+#      dict(Task="Job-2", Start='2017-01-17', Finish='2017-02-17', Resource='Not Started'),
+#      dict(Task="Job-2", Start='2017-01-17', Finish='2017-02-17', Resource='Complete'),
+#      dict(Task="Job-3", Start='2017-03-10', Finish='2017-03-20', Resource='Not Started'),
+#      dict(Task="Job-3", Start='2017-04-01', Finish='2017-04-20', Resource='Not Started'),
+#      dict(Task="Job-3", Start='2017-05-18', Finish='2017-06-18', Resource='Not Started'),
+#      dict(Task="Job-4", Start='2017-01-14', Finish='2017-03-14', Resource='Complete')]
 
 colors = {'Not Started': 'rgb(220, 0, 0)',
           'Incomplete': (1, 0.9, 0.16),
@@ -17,4 +24,3 @@ fig = ff.create_gantt(df, colors=colors, index_col='Resource', show_colorbar=Tru
                       group_tasks=True)
 
 fig
-#fig.show()
