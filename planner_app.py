5ec2d8ab5db721d7eca5452dac72cd0bb34f9ad9
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2020-12-30 11:48:51 +0000

    Update planner_app.py

--- planner_app.py
+++ planner_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objs as go
 
 #set the plan logging
 from datetime import datetime
@@ -19,7 +20,10 @@ st.write("Use the template csv file")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file",type=['CSV'])
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, encoding='latin1')
+    df = pd.read_csv(uploaded_file, header=[0], encoding='latin1')
+    df['Start'] = pd.to_datetime(df['Start'])
+    df['Finish'] = pd.to_datetime(df['Finish'])
+
 #    df['Start'] = df['Start'].astype('datetime64')
 #    df['Finish'] = df['Finish'].astype('datetime64')
     
@@ -33,8 +37,7 @@ if uploaded_file is not None:
 #    fig = ff.create_gantt(df, index_col='Status', show_colorbar=True,
 #                         showgrid_x=True, showgrid_y=True)
  
-    Fig = px.timeline(df)
-                      #, x_start="Start", x_end="Finish")
+    Fig = px.timeline(df, x_start="Start", x_end="Finish")
     # y="Task",
    #     color_discrete_sequence=px.colors.qualitative.Prism, opacity=.7, range_x=None,
   #                range_y=None,
@@ -47,4 +50,4 @@ if uploaded_file is not None:
  
     fig
     
-#    go.FigureWidget(fig)
+    go.FigureWidget(fig)
