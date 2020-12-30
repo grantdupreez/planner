37ec08455082ec9367190b7affdf641687311d35
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2020-12-30 10:54:49 +0000

    Update planner_app.py

--- planner_app.py
+++ planner_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.figure_factory as ff
+#import plotly.figure_factory as ff
+import plotly.express as px
 
 #set the plan logging
 from datetime import datetime
@@ -19,14 +20,29 @@ st.write("Use the template csv file")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file",type=['CSV'])
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, header=[0], parse_dates=[1])
+    df = pd.read_csv(uploaded_file, header=[0], encoding='latin1')
+    df['Start'] = df['Start'].astype('datetime64')
+    df['Finish'] = df['Finish'].astype('datetime64')
+    
+    orders = list(df['Process'])
+    
     st.write(df)
     
 #ACTION
 #    st.to_csv(s3_string+dt_string)
     
-    fig = ff.create_gantt(df, index_col='Status', show_colorbar=True,
-                         showgrid_x=True, showgrid_y=True)
+#    fig = ff.create_gantt(df, index_col='Status', show_colorbar=True,
+#                         showgrid_x=True, showgrid_y=True)
+ 
+    Fig = px.timeline(df, x_start="Start", x_end="Finish", Y="Task",
+        color_discrete_sequence=px.colors.qualitative.Prism, opacity=.7, , range_x=None
+                  , range_y=None
+                  , template='plotly_white'
+                  , height=1200, , color='Dimension'
+                  , title ="<b>IE 3.0 Gantt Chart 2021</b>"
+                  )
+ 
  
     fig
     
+    go.FigureWidget(fig)
