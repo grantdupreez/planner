8ca56f13e62d4c238acb4c55b12599412e21d351
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2020-12-30 15:38:54 +0000

    Update planner_app.py

--- planner_app.py
+++ planner_app.py
@@ -41,8 +41,8 @@ if uploaded_file is not None:
                       , x_end="Finish"
                       , y="CR"
                       , hover_name="CR"
-                      , color='Color'
-                      , opacity=.7
+#                      , color='Color'
+#                      , opacity=.7
     )
     
     fig.update_yaxes(autorange="reversed")     
