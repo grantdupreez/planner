8834926cda1bbfe8ca26fbcf1e2f9390080858f6
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2020-12-30 11:26:50 +0000

    Update planner_app.py

--- planner_app.py
+++ planner_app.py
@@ -21,7 +21,7 @@ st.write("Use the template csv file")
 uploaded_file = st.sidebar.file_uploader("Choose a file",type=['CSV'])
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, header=[0], encoding='latin1')
-#    df['Start'] = df['Start'].astype('datetime64')
+    df['Start'] = df['Start'].astype('datetime64[ns]')
 #    df['Finish'] = df['Finish'].astype('datetime64')
     
 #    orders = list(df['Process'])
@@ -34,15 +34,15 @@ if uploaded_file is not None:
 #    fig = ff.create_gantt(df, index_col='Status', show_colorbar=True,
 #                         showgrid_x=True, showgrid_y=True)
  
-    Fig = px.timeline(df, x_start="Start", x_end="Finish", Y="Task")
-#    ,
-#        color_discrete_sequence=px.colors.qualitative.Prism, opacity=.7, range_x=None,
-#                  range_y=None,
-#                  template='plotly_white',
-#                  height=1200,
-#                  color='Dimension',
-#                  title="<b>Gantt Chart 2021</b>"
-#                  )
+    Fig = px.timeline(df, x_start="Start", x_end="Finish")
+    # y="Task",
+   #     color_discrete_sequence=px.colors.qualitative.Prism, opacity=.7, range_x=None,
+  #                range_y=None,
+  #                template='plotly_white',
+  #                height=1200,
+#                  color='Dimension')
+  #                title="<b>Gantt Chart 2021</b>"
+  #                )
  
  
     fig
