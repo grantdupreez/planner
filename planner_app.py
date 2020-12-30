4e66daa5945713d48029607e40e74f1a145be11f
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2020-12-30 12:51:49 +0000

    Update planner_app.py

--- planner_app.py
+++ planner_app.py
@@ -28,7 +28,7 @@ if uploaded_file is not None:
     df['Start'] = df['Start'].astype('datetime64')
     df['Finish'] = df['Finish'].astype('datetime64')
     
-    orders = list(df['Task'])
+#    orders = list(df['Task'])
     
     st.write(df)
     
@@ -38,7 +38,98 @@ if uploaded_file is not None:
 #    fig = ff.create_gantt(df, index_col='Status', show_colorbar=True,
 #                         showgrid_x=True, showgrid_y=True)
  
-    fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task")
+    
+    colors = {'Ariba' : 'rgb(30,144,255)'
+          , 'Ariba - Date TBD' : 'rgb(211,211,211)'
+          , 'S4' : 'rgb(95,158,160)'
+          , 'Network' : 'rgb(0,0,128)'
+          , 'Network - Date TBD' : 'rgb(211,211,210)'}
+    
+    fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", hover_name="Task",
+                     color_discrete_sequence=px.colors.qualitative.Prism
+                  , opacity=.7
+                     , range_x=None
+                  , range_y=None
+                  , template='plotly_white'
+                  , height=1200
+                     , color='Dimension'
+                  , title ="<b>IE 3.0 Gantt Chart 2021</b>"
+                   , color=colors
+                 )
+    
+            fig.update_layout(
+            bargap=0.5
+            ,bargroupgap=0.1
+            ,xaxis_range=[df.Start.min(), df.Finish.max()]
+            ,xaxis = dict(
+                showgrid=True
+                ,rangeslider_visible=True
+                ,side ="top"
+                ,tickmode = 'array'
+                ,dtick="M1"
+                ,tickformat="Q%q %Y \n"
+                ,ticklabelmode="period"
+                ,ticks="outside"
+                ,tickson="boundaries"
+                ,tickwidth=.1
+                ,layer='below traces'
+                ,ticklen=20
+                ,tickfont=dict(
+                    family='Old Standard TT, serif',size=24,color='gray')
+                ,rangeselector=dict(
+                    buttons=list([
+                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                        dict(count=6, label="6m", step="month", stepmode="backward"),
+                        dict(count=1, label="YTD", step="year", stepmode="todate"),
+                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                        dict(step="all")
+                    ])
+                    ,x=.37
+                    ,y=-.05
+                    ,font=dict(
+                        family="Arial",
+                        size=14,
+                        color="darkgray"
+            )))
+            
+            ,yaxis = dict(
+                title= ""
+                ,autorange="reversed"
+                ,automargin=True
+        #         ,anchor="free"
+                ,ticklen=10
+                ,showgrid=True
+                ,showticklabels=True
+                ,tickfont=dict(
+                    family='Old Standard TT, serif', size=16, color='gray'))
+            
+            ,legend=dict(
+                orientation="h"
+                ,yanchor="bottom"
+                ,y=1.1
+                ,title=""
+                ,xanchor="right"
+                ,x=1
+                ,font=dict(
+                    family="Arial"
+                    ,size=14
+                    ,color="darkgray"))
+        )
+        fig.update_traces( #marker_color='rgb(158,202,225)'
+                           marker_line_color='rgb(8,48,107)'
+                          , marker_line_width=1.5, opacity=0.95)
+        fig.update_layout(
+            title="<b>IE 3.0 Gantt Chart 2021</b>",
+            xaxis_title="",
+        #     margin_l=400,
+            yaxis_title="Initiatives",
+        #     legend_title="Dimension: ",
+            font=dict(
+                family="Arial",
+                size=24,
+                color="darkgray"
+            )
+        )
  
     fig
     
